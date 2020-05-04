69e2e1e92a7e573ce6f80256f418d439141078bd
18045262 <18045262@myrp.edu.sg>
2020-05-04 10:44:31 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoimageview/MainActivity.java
@@ -0,0 +1,24 @@
+package sg.edu.rp.c346.demoimageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView ivDay2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ivDay2 = findViewById(R.id.imageView2);
+        ivDay2.setImageResource(R.drawable.day2);
+
+        ImageView ivDay3 = findViewById(R.id.imageView3);
+        ivDay3.setImageResource(R.drawable.day3);
+
+
+    }
+}
